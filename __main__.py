c920471c52f7a81d9707c878c9db4558863697d4
Hristomir Todorov <hristomir.todorov@gmail.com>
2021-03-26 16:46:20 +0200

    Changed structure

--- __main__.py
+++ __main__.py
@@ -1,10 +1,14 @@
 from flask import request, redirect, render_template
 from flask import Flask
+
+import os
 import PyPDF2
 import requests
-import boto3
+from dotenv import load_dotenv
 
-app = Flask(__name__)
+from resources.upload import UploadFile
+
+app = Flask(__name__, template_folder='templates')
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -40,37 +44,17 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ('pdf',)
 
 
-def write_file(filename, file_data):
-    with open(filename, 'w') as file:
-        file.write(file_data)
-
-
 def read_file(file):
     read_pdf = PyPDF2.PdfFileReader(file)
-    number_of_pages = read_pdf.getNumPages()
-    file_len = 1000
-    counter = 1
-
-    page_content = ''
-    for page in range(number_of_pages):
-        raw_page_content = read_pdf.getPage(page)
-        page_content += raw_page_content.extractText()
-
-    file_data_list = [page_content[i:i + file_len] for i in range(0, len(page_content), file_len)]
-    for file_data in file_data_list:
-        filename = f'{counter}.txt'
-        write_file(filename, file_data)
-        counter += 1
+    page = read_pdf.getPage(0)
+    page_content = page.extractText()
 
     return page_content
 
 
 def post_to_aws(text):
     return True
-    # todo
-
-    client = boto3.client('polly')
-    aws_url = 'https://a.w.s'
+    aws_url = os.environ.get('AWS_URL')
     result = requests.post(aws_url, data=text)
 
     if result.status_code in (200, 201):
@@ -79,5 +63,11 @@ def post_to_aws(text):
     return False
 
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    load_dotenv()
+    app_host = os.environ.get('APP_HOST')
+    app_port = os.environ.get('APP_PORT')
+
+    app.add_url_rule('/upload', view_func=UploadFile.as_view('post'))
+
+    app.run(host=app_host, port=app_port)
